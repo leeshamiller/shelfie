12a14d79f4951ff64aaa8fb6946122c9390c98e7
Leesha Miller <leesha.miller11@gmail.com>
2018-12-14 10:28:23 -0700

    React set-up

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Product from '../product/Product'
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                Dashboard
+                <Product />
+            </div>
+        )
+    }
+}
+
+export default Dashboard
